72a3e647abbaae5b4cd7eb782d757c6c69708ec3
Obimbo07 <obimboausts@gmail.com>
2024-04-15 19:35:58 +0300

    added configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,5 +68,5 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
-  config.hosts << /[a-z0-9]+\.ngrok\.io/
+  config.hosts << "ce0e-105-163-38-15.ngrok-free.app"
 end
